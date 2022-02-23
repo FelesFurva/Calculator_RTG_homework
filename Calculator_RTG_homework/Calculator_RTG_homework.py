f86304f254e9926b8bbbfa7a4262e48e859a8468
FelesFurva <kitten.solar@gmail.com>
2022-02-23 10:51:07 +0200

    Added try except for vaue error

--- Calculator_RTG_homework/Calculator_RTG_homework.py
+++ Calculator_RTG_homework/Calculator_RTG_homework.py
@@ -2,49 +2,84 @@
 import math
 
 while True:
-    operator = input("1 Addition \n2 Subtraction \n3 Multiplication \n4 Devision \n5 Rasing power \n6 Sqr root \n\nQ/q to Exit \n\n :")
+    operator = input("\n1 Addition \n2 Subtraction \n3 Multiplication \n4 Devision \n5 Squer \n6 Sqr root \n\nQ/q to Exit \n\n :")
 
     if operator == 'q'  or operator == 'Q':
         break
 
     if operator == "1":
-
-        x = float(input("Number 1 : "))
-        y = float(input("Number 2 : "))
-
-        print('{} + {} = '.format(x, y))
-        print(x + y)
-
+        while True:
+            try:
+                x = float(input("Number 1 : "))
+                y = float(input("Number 2 : "))
+            except ValueError:
+                print("Please enter a number")
+            else:
+                 print(x, "+", y, "=", x + y)
+                 break  
+            
     elif operator == "2":
-
-        x = float(input("Number 1 : "))
-        y = float(input("Number 2 : "))
-        print(x - y)
+        while True:
+            try:
+                x = float(input("Number 1 : "))
+                y = float(input("Number 2 : "))
+            except ValueError:
+                print("Please enter a number")
+            else:
+                print(x, "-", y, "=", x - y)
+                break
 
     elif operator == "3":
 
-        x = float(input("Number 1 : "))
-        y = float(input("Number 2 : "))
-        print(x * y)
+        while True:
+            try:
+                x = float(input("Number 1 : "))
+                y = float(input("Number 2 : "))
+            except ValueError:
+                print("Please enter a number")
+            else:
+                print(x, "*", y, "=", x * y)
+                break
 
     elif operator == "4":
 
-        x = float(input("Number 1 : "))
-        y = float(input("Number 2 : "))
-        if y != 0:
-            print(x / y)
-        else:
-            print("cannot devide by zero!")
+        while True:
+            try:
+                x = float(input("Number 1 : "))
+                y = float(input("Number 2 : "))
+            except ValueError:
+                print("Please enter a number")
+            else:
+                if y != 0:
+                    print(x, "/", y, "=", x / y)
+                    break
+                else:
+                    print("cannot devide by zero!")
 
     elif operator == "5":
 
-        x = float(input("Number: "))
-        print(x**2)
+         while True:
+            try:
+                x = float(input("Number 1 : "))
+            except ValueError:
+                print("Please enter a number")
+            else:
+                print("The squer of", x, "=", math.pow(x, 2))
+                break
 
     elif operator == "6":
 
-        x = float(input("Number: "))
-        print(math.sqrt(x))
+         while True:
+            try:
+                x = float(input("Number 1 : "))
+            except ValueError:
+                print("Please enter a number")
+            else:        
+                if x > 0:
+                    print(math.sqrt(x))
+                    break
+                else:
+                    print("Must be a positve integer \n")
 
     else:
         print("Can't do that, pls try again")
